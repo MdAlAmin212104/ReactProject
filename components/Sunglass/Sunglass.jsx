58010a836ddb333b2cd3172f274cc981570d0937
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-03-11 21:59:03 +0600

    first commit

---
+++ components/Sunglass/Sunglass.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sunglass = () => {
+      return (
+            <div>
+                  <h3>this is sunglass components</h3>
+            </div>
+      );
+};
+
+export default Sunglass;
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Sunglass from '../components/Sunglass/Sunglass'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    <h1>this is react app</h1>
+    <Sunglass/>
+      
+    </>
+  )
+}
+
+export default App
