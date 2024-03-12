650af775aee8faf61c7ba70da221e2cda1785cbb
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-03-12 20:03:58 +0600

    other file added and create function

--- components/Sunglass/Sunglass.jsx
+++ components/Sunglass/Sunglass.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import './Sunglass.css'
+import Watch from '../Watch/Watch';
+import add from '../add';
+import { add, multiply } from '../add';
 
 const Sunglass = () => {
+      const first = 10;
+      const second = 10;
+      // const sum = add(first, second)
+      // const multiply = multiply(first, second)
+      const sum = add(first, second)
+      const result = multiply(first, second)
       return (
             <div>
                   <h3>this is sunglass components</h3>
+                  <Watch/>
             </div>
       );
 };
